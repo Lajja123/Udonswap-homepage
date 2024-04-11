f3483f126f6094329fdc1733923efa2276494e07
Lajja123 <lajja.lamprostech@gmail.com>
2024-04-11 18:33:33 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,44 @@
+// pages/index.js
 import Image from "next/image";
 import styles from "./page.module.css";
+import flower from './flower.png'
+import star from './star.svg'
+import udonswap from './Udonswap.gif'
+import Navbar from './navbar'
 
 export default function Home() {
   return (
+    <>
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <Navbar />
+      <div className={styles.boxContainer1}>
+        <div className={styles.text1}>
+          Crafting Seamless Trades, <br />
+          Bowl by Bowl <Image src={star} alt="shine" className={styles.star} />
+       </div>
+       <div className={styles.textsmall}>
+          Crafting <br />Seamless <Image src={star} alt="shine" className={styles.star} /> 
+          <br />Trades,
+          Bowl <br /> by Bowl 
+       </div>
+      </div>
+
+      <div className={styles.boxContainer2}>
+        <div className={styles.left}>
+        <Image src={udonswap} loading="lazy"  alt="logo" className={styles.tokenImg} />
+        </div>
+
+        <div className={styles.right}>
+          <div className={styles.text2}>
+            Experience Seamless Trading, <br /> Instant Liquidity, Unmatched <br />Security, Experience the Future of <br />Decentralized Finance.
+          </div>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
+    <div className={styles.flower}>
+    {/* <Image src={flower} alt="Box Image" className={styles.img} /> */}
+    </div>
+    </>
   );
 }
